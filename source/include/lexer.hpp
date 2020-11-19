cd315c02730a53497a2071477b1526e0a408cf78
ninjaoflight <jonguz5@hotmail.com>
2020-11-19 13:55:25 -0600

    constructor por defecto

--- source/include/lexer.hpp
+++ source/include/lexer.hpp
@@ -43,14 +43,16 @@ enum class TipoToken {
 	cond_switch,         // switch(cond)
 	cond_case,           // case <val>
 	cliclo_for,          // for(var;cond;incr)
-	ciclo_while          // while(cond)
+	ciclo_while,         // while(cond)
+	indefinido			 // otro / nulo
 };
 
 class TokenPos {
-	const size_t linea;
-	const size_t caracter;
+	const size_t linea = -1;
+	const size_t caracter = -1;
 
   public:
+	explicit TokenPos() = default;
 	TokenPos(size_t linea, size_t caracter)
 	    : linea(linea), caracter(caracter){};
 	const size_t getlinea() { return linea; }
@@ -58,13 +60,13 @@ class TokenPos {
 };
 
 class Token {
-	const TokenPos pos;
-	const TipoToken tipoToken;
-	const std::string_view contenido;
-
   public:
+	const TokenPos pos;
+	const TipoToken tipoToken = TipoToken::indefinido;
+	const std::string contenido;
+	explicit Token(){};
 	Token(const TokenPos &pos, TipoToken tipoToken,
-	      const std::string_view &contenido)
+	      const std::string &contenido)
 	    : pos(pos), tipoToken(tipoToken), contenido(contenido){};
 };
 } // namespace lexer
